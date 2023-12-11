4bedece9ba7c2ddee6433b36ad8dfa184f8828a2
Aiman Shamsiemon <maimansham@gmail.com>
2023-12-11 13:03:44 +0800

    change background image for header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+//install imagetools
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://maiman-1.github.io',
     base: '/astro-blog',
+    integrations: [astroImageTools]
 
 });
