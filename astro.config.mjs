2757fe200cb24eac8c59739730a132d1e7b2908c
Aiman Shamsiemon <maimansham@gmail.com>
2023-12-11 16:49:11 +0800

    Add astrofy layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 //install imagetools
 import { astroImageTools } from "astro-imagetools";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://maiman-1.github.io',
-    base: '/astro-blog',
-    integrations: [astroImageTools]
-
+  site: 'https://maiman-1.github.io',
+  base: '/astro-blog',
+  integrations: [astroImageTools, tailwind()]
 });
